4a33d5747f520c183b696c7e54ea7692d50af7f9
Matt Wilchek <mwilchek@gmu.edu>
2016-10-23 19:42:03 +0000

    \modified app.js

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -3,6 +3,14 @@ var app = express();
 
 var port = process.env.PORT;
 
+app.get('/', function(req, res){
+    res.send('Hello World');
+});
+
+app.get('/routing', function(req, res){
+    res.send('Hello Routing!');
+});
+
 app.listen(port, function(error){
     console.log('Server is running on port: ' + port);
 })
