05404b6a0f22526c824bd327dda2cf5b70993b48
Matt Wilchek <mwilchek@gmu.edu>
2016-10-23 19:18:09 +0000

    \Modified json

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,8 @@
-var hello = 'hello variables';
-console.log(hello);
+var express = require('express');
+var app = express(); 
+
+var port = process.env.PORT;
+
+app.listen(port, function(error){
+    console.log('Server is running on port: ' + port);
+})
